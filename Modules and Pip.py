82161ac577880441c1544ccacd84693b3cf935c3
imshivamchoudhary <shivamchoudhary012@gmail.com>
2024-12-21 15:48:41 +0530

    initial commit

---
+++ Modules and Pip.py
@@ -0,0 +1,10 @@
+# Modules:Modules are basically used to borrows someone else code which mean you can use someone else code in your program with the help of modules
+# module are 2 type
+# Built in modules
+# External modules
+# Pip is a package manger
+import _hashlib
+print('hello world')
+print(5)
+print('shivam',18)
+print(9*2)
