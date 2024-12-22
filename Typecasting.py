1322cf019aacf0674f3668e4aa9fa9aa84d94618
imshivamchoudhary <shivamchoudhary012@gmail.com>
2024-12-22 08:38:33 +0530

    initial commit

---
+++ Typecasting.py
@@ -0,0 +1,15 @@
+# typecasting : conversion of one data type into the other data type is known as typecasting
+# a='shivam'
+# b='bhai'
+# python support a wide varity of function or methods like:int(),float()
+# there are two type of typecasting
+# 1 explicit conversion: conversion through a developer or programmer manually as per requirement
+# it can be done by python built-in type
+a='2'
+b='3'
+print(a+b)
+print(int(a)+int(b))
+# 2 implicit conversion: according to the level one data type is converted into otherr by the python interpreter itself automatically
+s=23
+d=.1
+print(s+d)
