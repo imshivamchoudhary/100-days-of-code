1c6b1d408a448fdf22483d1168eb75b9fbdaf0d2
imshivamchoudhary <shivamchoudhary012@gmail.com>
2024-12-24 12:09:27 +0530

    initial commit and day7 of code

---
+++ strings.py
@@ -0,0 +1,20 @@
+# string:in python anything that you enclosed in double or single quotation marks is consederd as a string
+name="shivam"
+brother='sumit'
+print(name,brother)
+apple=('''he said
+hi shivam
+hey i am good''')
+print(len(apple))
+# print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+# print(name[6]) #throws an error
+print('lets us a for loop\n')
+
+for char in apple:
+    print(char)
