685d319713804401b87dfb59ffc79a1fb33c200c
imshivamchoudhary <shivamchoudhary012@gmail.com>
2024-12-24 21:21:43 +0530

    initial commit and day8 of code

---
+++ strings slicing and operators .py
@@ -0,0 +1,11 @@
+a='shivam,choudhary'
+print(a[0:6]) #including 0 but not 6
+print(a[:4])
+print(a[:])
+print(a[0:-10])
+print(a[0:len(a)-10])
+print(a[-2:-1])
+# length of a string
+# we can find length of a string using len() function
+print(len(a))
+# for slicing we can use square bracket --->[]
