26a2609930040b94497dd1a62aa31592774471b4
imshivamchoudhary <shivamchoudhary012@gmail.com>
2024-12-24 12:07:35 +0530

    initial commit and day6 of code

---
+++ taking user input.py
@@ -0,0 +1,7 @@
+# we can take user input directly by using input()function
+a=input('enter your name: ')
+print('my name is',a)
+x=input('enter the first number: ')
+y=input('enter the second number: ')
+print(x+y)
+print(int(x)+int(y))
