602a3b2e41e037626dcb0421efe96c40c81e4a8d
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-01-22 21:34:42 +0530

    initial commit

--- day39 of code /if name equal to main.py
+++ day39 of code /if name equal to main.py
@@ -5,3 +5,8 @@
 # In the python __name__ variable is a built-in variable that is automatically set to the name of the current Module 
 # When a python script is run directly,,the __name__variable is set to the string __main__ when the script is imported 
 # as a module into another script,the __name__ variable is set to the name of the module
+import ishivam
+ishivam.welcome()
+
+# In this example , the main function contain the code that should be run when the script is run directly.
+# The if statement at the bottom check whether the __name__ variable is equal to __main__. If it is the main function is called. 
--- day39 of code /ishivam.py
+++ day39 of code /ishivam.py
@@ -1,5 +1,7 @@
 def welcome():
-    print('Hey you welcome from shivam')
+    print('Hey you are welcome from ishivam')
 sh='I am good boy'
-welcome()
 print(sh)
+if __name__=="__main__":
+    welcome()
+print(__name__)
