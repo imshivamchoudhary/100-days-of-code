687c0fb63917eb7bebd0aa01d5874f0a56c7d5a4
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-01-22 22:55:10 +0530

    It is a os module demo file

--- osm.py
+++ osm.py
@@ -1,7 +1,9 @@
 import os
 print(os.getcwd())
-if (not os.path.exist(f"day{i}")):
-    os.mkdir(f"day{i} of code")
 
-for i in range (40,101):
-    os.mkdir(f"day{i} of code")
+
+for i in range (1,5):
+    os.mkdir(f"test{i} of code")
+
+if (not os.path.exist(f"day{i} of code")):
+    os.mkdir(f"test{i} of code")
