8855ee42c6f4565f964aaab2fb9118e76227fa45
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-01-27 23:21:57 +0530

    initial commit

--- day45 of code/seek tell and other fuction.py
+++ day45 of code/seek tell and other fuction.py
@@ -18,3 +18,21 @@ print(d)
 
 # tell() function 
 # The tel() function return the current position within the file , in bytes. This can be usefull for keeping track
+# of your location within the file or for seeking to a specific position relative to the current position
+# print(f'it show the current byte position is {a.tell()}')
+with open('sh.txt','r') as a:
+    #read the first 10 bytes
+    d=a.read(10)
+    print(a.tell())
+    print(d)
+
+# truncate() function 
+# When you open a file in python using the open() function, you can specify the mode in which you want to open the file.
+# If you specify the mode as 'w' or 'a' the file is opened in write mode and you can write the file .however if you want 
+# to truncate the file to a specific size , you  can use the truncate function
+with open('sam.txt','w') as s:
+    s.write('''Hey Shivam ,What's up ?''')
+    s.truncate(5)
+with open('sam.txt','r') as s:
+    q=s.read()
+    print(q)
