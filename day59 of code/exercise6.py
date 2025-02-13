c5d64a6f8980e094be3426cc1461396259fa7335
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-02-14 00:01:15 +0530

    initial commit

--- day59 of code/exercise6.py
+++ day59 of code/exercise6.py
@@ -3,4 +3,18 @@
 # Show that your program doesnt persist the books after the program is stopped!
 
 class Library:
-    def __init__(self,):
+    def __init__(self):
+        self.noBooks=0
+        self.books=[]
+    def addBook(self,book):
+        self.books.append(book)
+        self.noBooks=len(self.books)
+    def showBooks(self):
+        print(f"Total number of books in the library is {self.noBooks} the books are")
+        for book in self.books:
+            print(book)
+e=Library()
+e.addBook('Python')
+e.addBook('Java')
+e.addBook('C++')
+e.showBooks()
