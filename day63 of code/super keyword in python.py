97e23d4457486b0eb23ac131e28186193f01858e
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-03-10 22:52:28 +0530

    initial commit

--- day63 of code/super keyword in python.py
+++ day63 of code/super keyword in python.py
@@ -2,4 +2,10 @@
 # The super() keyword in python  is used to refer to the parent class .It is especially useful when a class inherits from multiple parent classes and
 # you want to call a method from one of the parent class
 When a class inherits from a parent class , it can override or extend the methods defined in the parent class.However sometime you might want to use the parent class
-method in the child class . This s where 
+method in the child class . This s where the super() keywords comes in handy
+
+class parent(object):
+    def parent_method(self):
+        print("this is the  parent method ")
+
+class child(parent):    
